3eb58528af4b9481f301e6ee2c4a62143f617da6
Granger <fgranger@cdpq.com>
2018-03-21 20:21:45 -0400

    delete old but gold

--- hackrctest2-src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=hackrctest2-b035-Web-Deploy.pubxml -p:Password=Sx8iJDhmzreyKebplvrdup5AxJp5E9mSiw2RXuDPQi7LFiwHGaG5Jj5jeMET
-
