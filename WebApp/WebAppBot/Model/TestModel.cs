e2e38b270c84cafa663de47c69b53d87cfbdbddb
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2018-03-23 19:56:06 -0400

    Testing in progress ;)

--- WebApp/WebAppBot/Model/TestModel.cs
+++ WebApp/WebAppBot/Model/TestModel.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Net;
-using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Client;
-using Newtonsoft.Json;
-
 class TestMongoModel
 {
 }
