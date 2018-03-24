ff507f7af8ed40f14624e87b12272bbbe224943a
Granger <fgranger@cdpq.com>
2018-03-24 10:44:18 -0400

    modif avec lineup

---
+++ WebApp/WebAppBot/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppBot.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        [HttpGet("{text}")]
+        public string GetText([FromRoute]string text)
+        {
+            return "text api called";
+        }
+
+        [HttpGet("{vocal}")]
+        public string GetVocal([FromRoute]string vocal)
+        {
+            return "vocal api called";
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
