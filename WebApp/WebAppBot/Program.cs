21618df0d4c092b8ee7700f0a52e1dd298a3b996
Granger <fgranger@cdpq.com>
2018-03-25 00:15:57 -0400

    fix cgi shit

--- WebApp/WebAppBot/Program.cs
+++ WebApp/WebAppBot/Program.cs
@@ -18,6 +18,7 @@ namespace WebAppBot
 
         private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
